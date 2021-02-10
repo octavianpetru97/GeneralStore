b0b15ad9d47d61e0fe937cbc9dbc0ef57475e1dd
DESKTOP-13UA1BF\Tavi <octavianpetru97@yahoo.com>
2021-02-10 11:55:11 +0200

    Php Project

---
+++ customer/pay_offline.php
@@ -0,0 +1,38 @@
+<center><!--  center Begin  -->
+    
+    <h1> Pay Offline use Method </h1>
+    
+    <p class="text-muted">
+        
+        If you have any questions, feel free to <a href="../contact.php">Contact Us</a>. Our Customer Service work <strong>24/7</strong>
+        
+    </p>
+    
+</center><!--  center Finish  -->
+
+
+<hr>
+
+
+<div class="table-responsive"><!--  table-responsive Begin  -->
+    
+    <table class="table table-bordered table-hover table-striped"><!--  table table-bordered table-hover Begin  -->
+        
+        <thead><!--  thead Begin  -->
+           
+           <th> Bank Account Details: </th>
+           <th> Easy Paisa, UBL Omni, Mobi Cash Details: </th>
+           <th> Western Union Details </th> 
+            
+        </thead><!--  thead Finish  -->
+        
+        <tbody><!--  tbody Begin  -->
+
+        <td> Bank Name: UBL | Account No:182-131-443 | Branch Name: Lahore | Branch Code:1397 </td>
+        <td> NIC #980-234-221 | Mobile No: 0765213253 | Name: Octavian</td> 
+        <td>Real Name: Gheorghe Octavian | Mobile No: 0765213253 Country: Romania |  NIC #980-234-221 </td>
+        </tbody><!--  tbody Finish  -->
+        
+    </table><!--  table table-bordered table-hover Finish  -->
+    
+</div><!--  table-responsive Finish  -->
